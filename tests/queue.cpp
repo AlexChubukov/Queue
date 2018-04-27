afd1a43e90ad144cb46288ca75a37a737c7daf74
AlexDeveloper24 <alexchubukov12345@gmail.com>
2018-04-27 22:15:56 +0300

    modified

--- tests/queue.cpp
+++ tests/queue.cpp
@@ -1,45 +1,49 @@
 #include <catch.hpp>
-#include <sstream>
-#include <fstream>
-#include "queue.h"
-using namespace std;
-
-TEST_CASE("varios queues")
-{
-    Queue <int> q1;
-    Queue <int> q2 = {1, 2, 3, 4};
-    Queue <int> q3{q2};
-    REQUIRE( q2 == q3);
-}
-
-TEST_CASE("swap")
-{
-    Queue <int> q1;
-    Queue <int> q2 = {1, 2, 3, 4};
-    Queue <int> q3{q2};
-    Queue <int> temp = q3;
-    q1.swap(q3);
-    REQUIRE( q1 == temp);
-    REQUIRE( q3.empty() == true);
-    REQUIRE( q1.size() == 4);
-}
+#include "queue.cpp"
 
 TEST_CASE("push")
 {
-    Queue <int> q1;
-    Queue <int> q2 = {1, 2, 3, 4};
-    Queue <int> q3{q2};
-    q3.push(5);
-    REQUIRE( q3.size() == 5);
+ Queue<int> queue;
+ queue.push(1);
+ REQUIRE( queue.front() == 1);
+ REQUIRE( queue.back() == 1);
+ queue.push(2);
+ REQUIRE( queue.front() == 1);
+ REQUIRE( queue.back() == 2);
+ queue.push(3);
+ REQUIRE( queue.front() == 1);
+ REQUIRE( queue.back() == 3);
 }
-TEST_CASE("pop#1")
+
+TEST_CASE("pop")
 {
-    Queue<int> q4;
-    q4.pop();
-    REQUIRE( q4.size() == 0);
-	q4.push(1);
-	q4.pop();
-	REQUIRE( q4.size() == 0);
+ Queue<int> queue;
+ queue.push(1);
+ queue.push(2);
+ queue.push(3);
+ int tmp = queue.front();
+ REQUIRE (tmp == 1);
+ queue.pop();
+ tmp = queue.front();
+ REQUIRE (tmp == 2);
+ queue.pop();
+ tmp = queue.front();
+ REQUIRE (tmp == 3);
+ queue.pop();
+ REQUIRE(queue.size()==0);
+}
+
+TEST_CASE("copy")
+{
+ Queue<int> queue1;
+ queue1.push(1);
+ queue1.push(2);
+ queue1.push(3);
+ 
+ Queue<int> queue2{queue1};
+ REQUIRE( queue2.front() == 1);
+ REQUIRE( queue2.back() == 3);
+ 
 }
 
 
